7f1cafea80ae39bb2b43e3322032022588bfb760
Karmehr Arora <karmehr.arora@gmail.com>
2025-10-23 16:08:35 -0400

    completed date time processing for comment & video data

--- scripts/sprint4/data_quality.sh
+++ scripts/sprint4/data_quality.sh
@@ -12,45 +12,52 @@ DATA_QUALITY
 
 set -euo pipefail
 
-HOME_DIR="$(cd ../.. && pwd)"
-OUTPUT_DIR="${HOME_DIR}/out" 
+HOME_DIR="$(cd ../.. && pwd)" 
 DATASET_DIR="/mnt/scratch/CS131_jelenag/projects/team10_sec3/team10-youtube-socialmedia/data"
+OUTPUT_DIR="${DATASET_DIR}/processed"
 COMMENT_CSV="${DATASET_DIR}/yt_comments.csv"
 VIDEO_CSV="${DATASET_DIR}/yt_video_data.csv"
 
 # Verify directories are correct
 #echo "Home path: ${HOME_DIR}"
-#echo "Output directory: ${OUTPUT_DIR}"
+echo "Output directory: ${OUTPUT_DIR}"
 #echo "Dataset directory: ${DATASET_DIR}"
 #echo "Comment CSV Path: ${COMMENT_CSV}"
 #echo "Video CSV Path: ${VIDEO_CSV}"
 
 cd "${OUTPUT_DIR}"
+#set -x # for debugging purposes
 
-echo "comment date"
-#0
-#2025-09-27T11:35:03Z
-#UCfSgZSFXfp-eQqqZT9s
-#2025-09-27T11:33:09Z
-# awk -F',' -v OFS="," 'NR>1{ m=substr($4,1,7)}’ file
-#awk -F',' -v OFS="," 'NR==1 || m=substr($4,1,20) {}' "${COMMENT_CSV}"
-awk -F',' '{
+echo "separating comment date and time"
+awk -F',' 'BEGIN {OFS=","}
+NR==1{
+        print "comment_id", "author_display_name", "published_at", "like_count", "comment_text", "is_reply", "parent_id", "channel_id", "date", "time"
+        next
+}
+{
 	split($4, a, "T")
+	d=a[1]
 	t=substr(a[2],1,8)
-	if (a[1] ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ && t ~ /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/){
-		print "date " a[1], "and time", t 
+	if (d ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ && t ~ /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/){
+		print $0, d, t 
 	}
-}' "${COMMENT_CSV}" | head -n 10
+}' "${COMMENT_CSV}" > "yt_comments_datetime.csv" || true
+sed -i 's/\r//g' "yt_comments_datetime.csv"
 
-echo "video date"
-awk -F',' '{
+echo "seperating video date and time"
+awk -F',' 'BEGIN {OFS=","}
+NR==1{
+        print "kind", "channel_id", "default_language", "video_id", "title", "description", "publish_date", "view_count", "like_count", "comment_count", "live_broadcast_content", "tags", "duration", "definition", "caption", "date", "time"
+        next
+}
+{
         split($7, a, "T")
+        d=a[1]
         t=substr(a[2],1,8)
-        if (a[1] ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ && t ~ /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/){
-                print "date " a[1], "and time", t
+        if (d ~ /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/ && t ~ /^[0-9]{2}:[0-9]{2}:[0-9]{2}$/){
+                print $0, d, t
         }
-}' "${VIDEO_CSV}" | head -n 10
-#awk -F',' '{ split($7, a, "T"); t=substr(a[2],1,8);print "date " a[1], "and time", t }' "${VIDEO_CSV}" | head -n 10
-#cut -f7 "${VIDEO_CSV}" | head -n 10
+}' "${VIDEO_CSV}" > "yt_video_datetime.csv" || true
+sed -i 's/\r//g' "yt_video_datetime.csv"
 
-
+echo "operation completed"
